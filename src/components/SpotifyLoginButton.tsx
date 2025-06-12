d0dcd039c191aae8286c9cb4091e49f880cf0f51
Brady Winslow <97055964+bradywinslow@users.noreply.github.com>
2025-06-11 18:46:24 -0700

    move spotify-icon.svg to public folder

--- src/components/SpotifyLoginButton.tsx
+++ src/components/SpotifyLoginButton.tsx
@@ -44,7 +44,7 @@ export default function SpotifyLoginButton({ className }: SpotifyLoginButtonProp
                 <div onClick={handlePkceLogin}>
                     {!isLoggedIn && 
                         <button className='w-full flex items-center justify-center gap-2 px-4 py-2 rounded-xl font-semibold bg-[#1DB954] text-black hover:bg-[#1ed760] active:bg-[#1aa34a] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#1DB954] text-center mt-6'>
-                            <img src='src/assets/spotify-icon.svg' alt='Spotify' className='w-5 h-5' />
+                            <img src='/spotify-icon.svg' alt='Spotify' className='w-5 h-5' />
                             Continue with Spotify to Search
                         </button>
                     }
